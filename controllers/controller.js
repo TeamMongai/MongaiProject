1fc37e17c21a6d55854d4bd4cd79ba7a39979c8a
ben grosser <bgrosser0@mncallysmith.edu>
2018-04-19 14:40:39 -0500

    end of thursday

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,1 +1,14 @@
+var express = require("express");
 
+var router = express.Router();
+
+var model = require("../models/model.js")
+
+router.get("/", function(req, res) {
+        var hbsObject = {
+        };
+        console.log(hbsObject);
+        res.render("user", hbsObject);
+    });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 var app = express();
 
@@ -22,6 +22,7 @@ app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
 //var routes = require(controllers);
+var routes = require("./controllers/controller.js");
 
 app.use(routes);
 
