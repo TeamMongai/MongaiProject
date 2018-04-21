fbd1f83677231c095bc0a21e29735c908c6b488c
Mark Mankey <mark.mankey@gmail.com>
2018-04-21 14:54:43 -0500

    sequalize conversion 1 complete, need to test with DB

--- controllers/userLoginController.js
+++ controllers/userLoginController.js
@@ -6,7 +6,7 @@ var router = express.Router();
 
 var db = require("../models/") // I think with sequalize we need to just require models...
 
-//May need to move checkUserChapterValue() inside the router statements due to email scope issues
+//May need to move checkUserChapterValue() inside the router statements due to email scope issues - need to test
 function checkUserChapterValue(email) {
     connection.query('SELECT * FROM users WHERE email = ?', [email], function (error, results, fields) {
         console.log("userLoginController: checkUserChapterValue: checking userChapterLocation");
@@ -24,18 +24,17 @@ function checkUserChapterValue(email) {
 
 
 
-// LOGIN - NOT YET CONVERTED TO SEQUALIZE - Converting
+// LOGIN - CONVERTED TO SEQUALIZE - Need to test
 router.get("/login", function (req, res) {
     var email = req.body.email; //User input email
     var password = req.body.password; //User input password
     console.log("userLoginController: Checking user login: ", email, password)
-
-
-
-
-
-
-    connection.query('SELECT * FROM users WHERE email = ?', [email], function (error, results, fields) {
+    //Sequalized query
+    db.userDataModel.findOne({
+        where: {
+            email: req.body.email
+        },
+    }).then(function(res) {
         console.log("userLoginController: Password from DB ", results[0].password);
         if (error) {
             res.send({
@@ -74,7 +73,52 @@ router.get("/login", function (req, res) {
 });
 
 
-// REGISTER - Need to test
+
+
+
+
+//  OLD NON SEQUALIZE CODE THAT WAS TESTED AND WORKED
+
+//     connection.query('SELECT * FROM users WHERE email = ?', [email], function (error, results, fields) {
+//         console.log("userLoginController: Password from DB ", results[0].password);
+//         if (error) {
+//             res.send({
+//                 "code": 400,
+//                 "failed": "error ocurred"
+//             })
+//         } else {
+//             if (results.length > 0) {
+//                 if (results[0].password == password) {
+//                     console.log("userLoginController: login sucessfull")
+//                     res.send({
+//                         "code": 200,
+//                         "success": "userLoginController: login sucessfull"
+
+//                     });
+//                     //////////
+//                     // Check user chapter value in DB and direct user to that chapter
+//                     ///////////
+//                     checkUserChapterValue(email); // Function below checks the user chapter value, may need to move this inside the router statments due to scope
+//                 }
+//                 else {
+//                     res.send({
+//                         "code": 204,
+//                         "success": "userLoginController: Email and password does not match"
+//                     });
+//                 }
+//             }
+//             else {
+//                 res.send({
+//                     "code": 204,
+//                     "success": "userLoginController: Email does not exits"
+//                 });
+//             }
+//         }
+//     });
+// });
+
+
+// REGISTER - Converted to sequalize- Need to test
 router.post('/register', function (req, res) {
     db.userDataModel.create({
         email: req.body.email,
@@ -97,7 +141,7 @@ router.post('/register', function (req, res) {
         //////////
         // Check user chapter value in DB and direct user to that chapter
         ///////////
-        checkUserChapterValue(email); // Function below checks the user chapter value, may need to move this inside the router statments due to scope
+        checkUserChapterValue(email); // checkUserChapterValue() checks the user chapter value, may need to move this inside the router statments due to scope
     }
 });
 
