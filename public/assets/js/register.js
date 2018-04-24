5f7658c95dcaf038ea534fe932c239007c5bbb61
Mark Mankey <mark.mankey@gmail.com>
2018-04-24 10:32:37 -0500

    register.js just starting, it will eventually send json requests to the userLoginController.js

---
+++ public/assets/js/register.js
@@ -0,0 +1,9 @@
+console.log("register.js: register.js loaded on client!")
+$(document).ready(function() {
+    $(document).on("click", "button.submit", submitRegister);
+});
+
+function submitRegister(event) {
+    console.log("register.js: Submit Registration ");
+    
+};
